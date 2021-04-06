782903c2114e084111c81ee921b8339f2993760e
Orville Dillon <60524259+eLEPHANTCase@users.noreply.github.com>
2021-04-05 23:27:01 -0400

    Fix: Fixed the screenshots!

--- app/app.py
+++ app/app.py
@@ -32,6 +32,12 @@ def index() -> str:
     resp = Response(js, status=200, mimetype='application/json')
     return resp
 
+@app.route('/api/cities')
+def cities() -> str:
+    js = json.dumps(cities_import())
+    resp = Response(js, status=200, mimetype='application/json')
+    return resp
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
